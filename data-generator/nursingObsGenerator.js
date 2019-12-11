97010d5f21674d53229ed5d9e6702be80fad8c9b
sherpal8 <sherpalkorotana@icloud.com>
2019-12-11 12:16:31 +0000

    adding units to nursing observation data

--- data-generator/nursingObsGenerator.js
+++ data-generator/nursingObsGenerator.js
@@ -1,37 +1,39 @@
 "use strict";
 
-const pulseArray = [],
-  diastolicArray = [],
-  systolicArray = [],
-  respRateArray = [],
-  oxygenSaturationsArray = [],
-  urineOutputArray = [];
-
 const nursingObsGenerator = () => {
+  const obj = {};
+  obj.pulseArray = [];
+  obj.diastolicArray = [];
+  obj.systolicArray = [];
+  obj.respRateArray = [];
+  obj.oxygenSaturationsArray = [];
+  obj.urineOutputArray = [];
+
   for (let i = 0; i < 350; i++) {
     // generate pulse data
     if (0 <= i && i <= 240) {
-      pulseArray.push({ pulse: i });
+      obj.pulseArray.push({ pulse: `${i} b.p.m` });
     }
     // generate diastolic data
     if (0 <= i && i <= 160) {
-      diastolicArray.push({ diastolic: i });
+      obj.diastolicArray.push({ diastolic: `${i} mmHg` });
     }
     // generate systolic data
     if (40 <= i && i <= 260) {
-      systolicArray.push({ systolic: i });
+      obj.systolicArray.push({ systolic: `${i} mmHg` });
     }
     // generate respiratory_rate data
     if (0 <= i && i <= 40) {
-      respRateArray.push({ resp_rate: i });
+      obj.respRateArray.push({ resp_rate: `${i} breaths/min` });
     }
     // generate oxygen_saturations data
     if (0 <= i && i <= 100) {
-      oxygenSaturationsArray.push({ oxygen_sats: i });
+      obj.oxygenSaturationsArray.push({ oxygen_sats: `${i} % at 0.21 FiO2` });
     }
     // generate urine_output data
-    urineOutputArray.push({ urine_output: i });
+    obj.urineOutputArray.push({ urine_output: `${i} ml/hour` });
   }
+  return obj;
 };
 
 module.exports = { nursingObsGenerator };
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const { nursingObsGenerator } = require("./data-generator/nursingObsGenerator");
 
+const nursingObs = nursingObsGenerator();
+console.log(nursingObs);
+
+// for (let item in nursingObs) {
+//   console.log(`{ ${[item]}-length: ${nursingObs[item].length} }`);
+// }
