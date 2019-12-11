44b104b55185c59be74aca4d3556103e63dfbc35
sherpal8 <sherpalkorotana@icloud.com>
2019-12-11 13:08:48 +0000

    creating index file to collect modules

---
+++ data-generator/index.js
@@ -0,0 +1,11 @@
+const { nursingObsGenerator } = require("./nursingObsGenerator");
+const { temperatureGenerator } = require("./temperatureGenerator");
+const { urineOutputGenerator } = require("./urineOutputGenerator");
+const { bloodGlucometerGenerator } = require("./bloodSugarLevelGenerator");
+
+module.exports = {
+  nursingObsGenerator,
+  temperatureGenerator,
+  urineOutputGenerator,
+  bloodGlucometerGenerator
+};
