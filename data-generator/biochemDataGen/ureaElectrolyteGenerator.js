b1bc3d0485b70d7846e3016a5d488daeb9015ade
sherpal8 <sherpalkorotana@icloud.com>
2019-12-12 17:23:03 +0000

    generate sodium data range

--- data-generator/biochemDataGen/ureaElectrolyteGenerator.js
+++ data-generator/biochemDataGen/ureaElectrolyteGenerator.js
@@ -7,20 +7,30 @@ const ureaElectrolyteGenerator = () => {
   ureaElectrolyteObj.creatinine = [];
   ureaElectrolyteObj.eGFR = [];
   ureaElectrolyteObj.calcium = [];
+  ureaElectrolyteObj.magnesium = [];
   ureaElectrolyteObj.phosphate = [];
   ureaElectrolyteObj.bicarbonate = [];
   ureaElectrolyteObj.glucose = [];
-  for (let i = 0; i < 200; i++) {}
-  // Na+
-  // K+
-  // Cl-
-  // Urea
-  // Creatinine
-  // eGFR
-  // Calcium
-  // Phosphate
-  // HCO3-
-  // Glucose
+  ureaElectrolyteObj.serumOsmolality = [];
+  for (let i = 0; i < 200; i++) {
+    // Na+ (n: 133 – 146 mmol/L)
+    if (i >= 108 && i <= 168) {
+      ureaElectrolyteObj.sodium.push({ sodium: `${i} mmol/L` });
+    }
+    // K+ (n: 3.5 – 5.3 mmol/L)
+
+    // Cl- (n: 95 – 108 mmol/L)
+    // Urea (n: 2.5 – 8.2 mmol/L)
+    // Creatinine (n: 50-110 micro mol/ L)
+    // eGFR
+    // serum Osmolality (n: 275 – 295 mmol/kg)
+    // Calcium
+    // Magnesium (n: 0.65 – 1.05 mmol/L)
+    // Phosphate (n: 0.8 – 1.5 mmol/L)
+    // HCO3-
+    // Glucose
+  }
+  return ureaElectrolyteObj;
 };
 
 module.exports = { ureaElectrolyteGenerator };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const nursingObsObj = require("./data-generator/nursingObsDataGen");
 const biochemObj = require("./data-generator/biochemDataGen");
 // nursing obs object
-console.log(Object.keys(biochemObj));
+console.log(biochemObj.ureaElectrolyteData.sodium);
 
 // biochemistry value object
 // blood count and microscopy value object
